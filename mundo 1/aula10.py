1e74feda673609c3778dd89fd5883cf2ce3eb889
NicolasVitto <emaildonicolas36@gmail.com>
2025-06-02 20:23:44 -0300

    arrumando alguns codigos que estão dificeis de compreender

--- mundo 1/aula10.py
+++ mundo 1/aula10.py
@@ -1,9 +1,10 @@
-"""R = float(input("digite quanto você tem na carteira? R$:"))
+"""
+R = float(input("digite quanto você tem na carteira? R$:"))
 
 C = R / 5.54
 
-print("com {:.2f}R$ você podera comprar {:.2f} USD ".format(R, C))"""
-
+print("com {:.2f}R$ você podera comprar {:.2f} USD ".format(R, C))
+"""
 from util import receber_entrada
 
 
--- mundo 1/aula11.py
+++ mundo 1/aula11.py
@@ -1,6 +1,27 @@
+"""
 largura = float(input("coloque a largura da parede: "))
 altura = float(input("coloque a altura da parede: "))
 area = largura * altura
 tinta = area / 2
-print("sua parede tem a dimensão de {} x {} e sua área é de {:.2f}M².".format(largura, altura, area))
+print("sua parede tem a dimensão de {} x {} e sua área é de {:.2f}".".format(largura, altura, area))
 print("Para pintar a parede, você precisará de {:.2f}L de tinta.".format(tinta))
+"""
+from util import receber_entrada
+
+
+def area (largura, altura):
+   return largura * altura
+
+def tinta(largura,altura):
+    return area(largura, altura) / 2
+
+def main():
+    
+    largura = receber_entrada("qual a largura da parade: ")
+    altura = receber_entrada("qual a altura da parede: ")
+    
+    print(f"sua parede tem a dimensão de {largura} X {altura} e sua área é de {area(largura,altura):.2f}M²")
+    print(f"para pintar a parade você precisa de {tinta(largura,altura):.2f}L de tinta ")
+    
+if __name__== "__main__":
+    main()    
